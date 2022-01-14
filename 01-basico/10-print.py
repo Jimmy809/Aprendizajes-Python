da1d1d158ebefb345b85329cd71ef2a87293e384
Jimmy Reyes <jimmyreyes1983@hotmail.com>
2022-01-14 18:34:30 +0100

    prunt.py Agregado

---
+++ 01-basico/10-print.py
@@ -0,0 +1,37 @@
+# usamos print para mostrar en pantalla la informacion q solicitemos
+
+nombre = 'Marcos'
+print(nombre)
+
+nombres = '''Marcos
+Luis
+Rosa
+Carlos'''
+print('nombres')
+
+#para saber la contidad de caracteres que tiene un text
+print(len(nombre))
+
+# para mostrar varias veces el mismo mensaje
+print(nombre * 3)
+
+#para elegir una letra en especifico de un texto
+print(nombre[0])
+
+#para cortar una secuencia en orden inversa
+print(nombre [-4])
+
+#para mostrar solo un grupo en secuencia
+print (nombre [0:2])
+
+#para mostar palabras especificas
+
+
+# para mostrar todo en mayuscula
+print(nombre.upper())
+
+# para mostrar todo en minuscula
+print(nombre.lower())
+
+# para remplazar una letra o palabras
+print(nombre.replace('c', 'p'))
