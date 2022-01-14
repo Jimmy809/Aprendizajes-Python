eec1fc42d4306766d56d5162153dcb008aec5904
Jimmy Reyes <jimmyreyes1983@hotmail.com>
2022-01-14 15:24:03 +0100

    input.py Creado

---
+++ 01-Intoduccion/03-input.py
@@ -0,0 +1,10 @@
+# input es el comando utilizado para pedirle al usuario algun dato
+
+# Hacemos una pregunta
+print('Cual es tu nombre?')
+
+# le pedimos el dato y lo mandamos a la variable nombre
+nombre = input()
+
+# y lo imprimimos con una bienvenida
+print('Bienvenido ' + nombre)
