414d65e463d4edf087dd3d66aee58fb3a143e954
Jimmy Reyes <jimmyreyes1983@hotmail.com>
2022-01-14 15:57:07 +0100

    aritmetica.py Agregado

---
+++ 01-Intoduccion/04-aritmetica.py
@@ -0,0 +1,27 @@
+# Simbolos aritmetico
+
+a = 10
+b = 7
+
+# Sumar +
+print('Resultado de la suma', a+b)
+
+# Resta -
+print('Resultado de la resta', a-b)
+
+# Multiplicacion *
+print('Resultado de la multiplicacion',a*b)
+
+# Division /
+print('Resultado de la Division', a/b)
+
+# Resto de la division %
+print('Resto de la Division', a%b)
+
+# Exponencial **
+print('Resutado exponencial', a**b)
+
+
+
+
+
