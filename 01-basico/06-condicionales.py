7af5094d22f2d28e01efa4bc6ab0a72a0e224dbb
Jimmy Reyes <jimmyreyes1983@hotmail.com>
2022-01-14 16:22:22 +0100

    condicionales.py if elif y else Agregados

---
+++ 01-basico/06-condicionales.py
@@ -0,0 +1,12 @@
+# if traduccion es "si" ej: si a es mayor que 0 es un numero positivo
+
+a = 0
+
+if a > 0:
+    print('a es un numero positivo')
+# elif traduccion es "sino" ej: sino a es un numero negativo
+elif a < 0:
+    print('a es un numero negativo')
+# else traduccion es "entonces" ej a es igual a 0
+else:
+    print('a es igual a 0')
