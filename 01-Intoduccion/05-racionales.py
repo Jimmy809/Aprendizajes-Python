259088406654130d5ed60c378a9501fc25ea0d4c
Jimmy Reyes <jimmyreyes1983@hotmail.com>
2022-01-14 15:57:57 +0100

    operadores racionales.py Agregados

---
+++ 01-Intoduccion/05-racionales.py
@@ -0,0 +1,22 @@
+# Operadores Racionales
+
+a = 10
+b = 7
+
+# Mayor que >
+print('a es mayor que b?', a>b)
+
+# Menor que <
+print('a es menor que b?', a<b)
+
+# Igual a ==
+print('a es igual que b?',a==b)
+
+# No es igual a !=
+print('a es distinto que b?',a!=b)
+
+# Mayor o igual >=
+print('a es igual que b?',a>=b)
+
+#Menor o igual<=
+print('a es igual que b?',a<=b)
