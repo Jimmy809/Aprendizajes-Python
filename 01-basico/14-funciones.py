cbf94583b3f8a65db323d1be9779ae7ba25a87bf
Jimmy Reyes <jimmyreyes1983@hotmail.com>
2022-01-20 15:06:33 +0100

    14- funciones.py Agregado

---
+++ 01-basico/14-funciones.py
@@ -0,0 +1,34 @@
+# Para definir una funcion usaremos def mas su nombre y termianmos con :
+
+#def mifincion (argumento):
+#    codigo 
+#    return retorno
+
+#funcion de suma
+def suma (a, b):
+    return a + b
+
+print(suma(2, 5))
+
+# funcion de resta
+def suma (a, b):
+    return a - b
+
+print(suma(2, 5))
+
+# pedir al usuario 2 numero
+def suma (a, b):
+    print(a%b)
+n1 = int(input('intoduce un numero '))
+n2 = int(input('Introduce otro numero '))
+
+suma(n1, n2)
+    
+n = int(input().strip())
+n2 = 2
+    
+def resto (n, n2):    
+    resultado = n % n2
+    
+print('resultado')
+
