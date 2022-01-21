36295c8b84bdb9db6ae412f2460cc1ff94050bca
Jimmy Reyes <jimmyreyes1983@hotmail.com>
2022-01-21 22:07:22 +0100

    Funciones agregadas

--- 01-basico/14-funciones.py
+++ 01-basico/14-funciones.py
@@ -1,34 +1,45 @@
 # Para definir una funcion usaremos def mas su nombre y termianmos con :
-
-#def mifincion (argumento):
+# def mifincion (argumento):
 #    codigo 
 #    return retorno
 
 #funcion de suma
-def suma (a, b):
-    return a + b
+# def suma (a, b):
+#     return a + b
 
-print(suma(2, 5))
+# print(suma(2, 5))
 
-# funcion de resta
-def suma (a, b):
-    return a - b
+# # # funcion de resta
+# def suma1 (a, b):
+#     return a - b
 
-print(suma(2, 5))
+# print(suma1(2, 5))
 
-# pedir al usuario 2 numero
-def suma (a, b):
-    print(a%b)
-n1 = int(input('intoduce un numero '))
-n2 = int(input('Introduce otro numero '))
+# # pedir al usuario 2 numero
+# def resto1 (a, b):
+#      print(a%b)
+# n1 = int(input('intoduce un numero '))
+# n2 = int(input('Introduce otro numero '))
 
-suma(n1, n2)
+# resto1(n1, n2)
     
-n = int(input().strip())
-n2 = 2
+# n = int(input().strip())
+# n2 = 2
     
-def resto (n, n2):    
-    resultado = n % n2
+# def resto (n, n2):    
+#     resultado = n % n2
     
-print('resultado')
+# print('resultado')
 
+# # imprimir funciones sin el print
+
+# def mifuncion():
+#     print ('hola mundo') # en vez de return solicitamos un print
+    
+# mifuncion() # en vez del print escribimos la funcion
+
+# # ejemplo 2
+def suma2 (x, y):
+     print(x + y)
+
+suma2(10, 10)
