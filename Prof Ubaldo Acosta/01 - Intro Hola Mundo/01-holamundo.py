d51d81a6a91980b8b74eb36df858da3e50a9350c
Jimmy Reyes <jimmyreyes1983@hotmail.com>
2022-01-22 00:38:31 +0100

    01 - Hola Mundo inicio clase con Ubaldo Acosta

---
+++ Prof Ubaldo Acosta/01 - Intro Hola Mundo/01-holamundo.py
@@ -0,0 +1,4 @@
+# para crear nuestro primer hola mundo
+# para texto podemos usar '' y ""
+# para imprimir en consola usaremos la funcion print() y dentro de los parentesis ponemos lo que deseamos imprimir
+print('Hola Mundo')
