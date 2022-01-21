976262b7091e58ddc7684c387008f4a4cfdb9908
Jimmy Reyes <jimmyreyes1983@hotmail.com>
2022-01-21 22:24:18 +0100

    15-Modulos Agregado

---
+++ 01-basico/15-modulo.py
@@ -0,0 +1,10 @@
+# modulos python
+
+#importamos el archivo py que tenga la funcion o clase que queremos importar
+import Modulos
+
+# llamamos la funcion
+numero = int(input('Introduce un numero positivo o negativo: '))
+
+# aplicamos el modulo junto con el nombre de la funciones que queremos utilizar
+Modulos.numeropositivo(numero)
---
+++ 01-basico/Modulos.py
@@ -0,0 +1,7 @@
+def numeropositivo(x):
+    if x > 0:
+        print ('Tu numero es positivo')
+    elif x < 0:
+        print ('Tu numero es negativo')
+    else:
+        print (' Tu numero es 0')
