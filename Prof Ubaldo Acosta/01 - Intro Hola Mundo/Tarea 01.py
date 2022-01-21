bcf3cf8f30e458617ba72a6528b9e506729ae55d
Jimmy Reyes <jimmyreyes1983@hotmail.com>
2022-01-22 00:38:40 +0100

    tarea 1

---
+++ Prof Ubaldo Acosta/01 - Intro Hola Mundo/Tarea 01.py
@@ -0,0 +1,10 @@
+# Crear un programa para saludar desde Python
+#Metodo 1 basico
+print("Saludos desde Python")
+
+#Metodo 2 complejo
+nombre = input('Bienvenido, cual es tu nombre? ')
+def saludar():
+    print('Saludos', nombre, 'desde Python')
+    
+saludar()
